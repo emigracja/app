440f5d69609c7e0073c26771fe5afca2795c3bfd
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2025-03-06 22:47:37 +0100

    feat(ai): get stocks list from backend API

--- ai/app/backend_api.py
+++ ai/app/backend_api.py
@@ -9,39 +9,19 @@ logger = logging.getLogger(__name__)
 
 
 def get_stocks() -> list[Stock]:
-    # TODO: Fetch the list from backend API
-    return [
-        Stock(
-            id=uuid.uuid4(),
-            symbol="AAPL",
-            name="Apple Inc.",
-            description="Apple Inc. is an American multinational technology company that specializes in consumer electronics, computer software, and online services.",
-            city="Cupertino",
-            country="United States",
-        ),
-        Stock(
-            id=uuid.uuid4(),
-            symbol="MSFT",
-            name="Microsoft Corporation",
-            description="Microsoft Corporation is an American multinational technology company that produces computer software, consumer electronics, personal computers, and related services.",
-            ekd="Technology",
-            city="Redmond",
-            country="United States",
-        ),
-        Stock(
-            id=uuid.uuid4(),
-            symbol="AMZN",
-            name="Amazon.com Inc.",
-            description="Amazon.com Inc. is an American multinational technology company that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
-            ekd="Consumer Cyclical",
-            city="Seattle",
-            country="United States",
-        ),
-    ]
+    # TODO: Adapt it to the final URL once #3 is completed
+    # TODO: Move the backend URL to env 
+    logging.info("Attempting to fetch the list of stocks from the backend API.")
+    url = "http://backend:8080/stock"
+    response = requests.get(url)
+    response.raise_for_status()
 
+    # Transform response JSON to Stock objects
+    stocks = [Stock(**stock) for stock in response.json()]
+    return stocks
 
 def send_article_stock_impact(article_id: uuid.UUID, article_stock_impact: ArticleStockImpact) -> bool:
-    # TODO: Adapt it to the final schema once #6 is completed
+    # TODO: Adapt it to the final URL and schema once #6 is completed
     # TODO: Move the backend URL to env 
     logger.info(
         f"Notifying the backend API about stock {article_stock_impact.stock_id} impacting article {article_id} with {article_stock_impact.impact} impact due to reason '{article_stock_impact.reason}'."
@@ -54,7 +34,13 @@ def send_article_stock_impact(article_id: uuid.UUID, article_stock_impact: Artic
     }
 
     url = "http://backend:8080/article/impact"
-    response = requests.post(url, json=request_body)
+    try:
+        response = requests.post(url, json=request_body)
+    except Exception as e:
+        logger.warning(f"Failed to notify the backend API about stock {article_stock_impact.stock_id} impacting article {article_id} with {article_stock_impact.impact} impact due to reason '{article_stock_impact.reason}'.")
+        logger.exception(e)
+        return False
+
     if response.status_code > 399:
         logger.warning(
             f"Failed to notify the backend API about stock {article_stock_impact.stock_id} impacting article {article_id} with {article_stock_impact.impact} impact due to reason '{article_stock_impact.reason}'."
