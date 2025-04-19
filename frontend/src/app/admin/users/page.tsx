b35517995d958222e2ef5a0c234292543b5e9643
orzehhowski <filmyminecraftnayt@gmail.com>
2025-04-19 22:02:35 +0200

    feat(frontend): add search bar to users list view

--- frontend/src/app/admin/users/page.tsx
+++ frontend/src/app/admin/users/page.tsx
@@ -2,9 +2,56 @@
 
 import UserList from "@/components/admin/UserList";
 import useDataStore from "@/store/useDataStore";
+import { useEffect, useCallback, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { debounce } from "lodash";
 
 export default function NewsPage() {
   const users = useDataStore((store) => store.users);
+  const [filteredUsers, setFilteredUsers] = useState(users);
 
-  return <UserList users={users} />;
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const searchQuery = searchParams.get("search");
+
+    if (!searchQuery) {
+      setFilteredUsers(users);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    searchQuery
+      ? setFilteredUsers(
+          users.filter((user) => user.email.includes(searchQuery.toLowerCase()))
+        )
+      : setFilteredUsers(users);
+  }, [searchParams, users]);
+
+  const search = useCallback(
+    debounce((query: string) => {
+      const url = new URL(window.location.href);
+      url.searchParams.set("search", query);
+      window.history.pushState({}, "", url);
+    }, 50),
+    []
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    search(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="w-full p-2">
+        <input
+          value={searchParams.get("search") ?? ""}
+          onChange={handleChange}
+          className="p-2 w-full text-white border-white border-1 rounded-xl outline-none"
+          type="text"
+          placeholder="Search..."
+        />
+      </div>
+      <UserList users={filteredUsers} />;
+    </>
+  );
 }
--- frontend/src/app/stocks/page.tsx
+++ frontend/src/app/stocks/page.tsx
@@ -2,9 +2,9 @@
 
 import useDataStore from "@/store/useDataStore";
 import StockList from "@/components/stocks/StockList";
-import {useEffect, useCallback, useState} from "react";
-import { useSearchParams } from 'next/navigation';
-import { debounce } from 'lodash';
+import { useEffect, useCallback, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { debounce } from "lodash";
 
 export default function StockPage() {
   const stocks = useDataStore((state) => state.stocks);
@@ -14,32 +14,48 @@ export default function StockPage() {
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    const searchQuery = searchParams.get('search');
+    const searchQuery = searchParams.get("search");
 
-    if(!searchQuery) {
-        setFilteredStocks(stocks);
+    if (!searchQuery) {
+      setFilteredStocks(stocks);
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    searchQuery ? setFilteredStocks(stocks.filter((stock) => stock.title.toLowerCase().includes(searchQuery.toLowerCase()))) : setFilteredStocks(stocks);
-
+    searchQuery
+      ? setFilteredStocks(
+          stocks.filter((stock) =>
+            stock.title.toLowerCase().includes(searchQuery.toLowerCase())
+          )
+        )
+      : setFilteredStocks(stocks);
   }, [searchParams]);
 
-  const search = useCallback(debounce((query: string) => {
-    const url = new URL(window.location.href);
-    url.searchParams.set('search', query);
-    window.history.pushState({}, '', url);
-  }, 300), []);
+  const search = useCallback(
+    debounce((query: string) => {
+      const url = new URL(window.location.href);
+      url.searchParams.set("search", query);
+      window.history.pushState({}, "", url);
+    }, 300),
+    []
+  );
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     search(e.target.value);
   };
 
-  return <>
-    <div className='w-full p-2'>
-      <input value={ searchParams.get('search') ?? ''} onChange={handleChange} className='p-2 w-full text-white border-white border-1 rounded-xl outline-none' type="text" placeholder="Search..." />
-    </div>
+  return (
+    <>
+      <div className="w-full p-2">
+        <input
+          value={searchParams.get("search") ?? ""}
+          onChange={handleChange}
+          className="p-2 w-full text-white border-white border-1 rounded-xl outline-none"
+          type="text"
+          placeholder="Search..."
+        />
+      </div>
 
-    <StockList stocks={filteredStocks} />
-  </>;
+      <StockList stocks={filteredStocks} />
+    </>
+  );
 }
