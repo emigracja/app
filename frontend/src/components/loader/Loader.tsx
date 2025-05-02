b33ec4ea2520c59e860749b7f6f67c66926b4e36
MRSIMM0 <s.pasieczny02@gmail.com>
2025-05-02 14:18:44 +0200

    fix: Added loader

---
+++ frontend/src/components/loader/Loader.tsx
@@ -0,0 +1,8 @@
+import {MoonLoader} from "react-spinners";
+import React from "react";
+
+const Loader = () => <section className="flex w-full h-full align-middle justify-center">
+    <MoonLoader className="mt-auto mb-auto" color="white" />
+</section>
+
+export default Loader;
