86cc5c305842d601b907cc8461e550f1f339c293
Szymon Pasieczny <44839807+MRSIMM0@users.noreply.github.com>
2025-06-08 18:38:58 +0200

    chore(backend, frontend): Cleanup (#111)

--- backend/src/main/java/com/example/backend/infrastructure/database/repository/WebPushSubscriptionRepository.java
+++ backend/src/main/java/com/example/backend/infrastructure/database/repository/WebPushSubscriptionRepository.java
@@ -3,6 +3,7 @@ package com.example.backend.infrastructure.database.repository;
 import com.example.backend.domain.service.webpush.subscription.WebPushSubscriptionService;
 import com.example.backend.infrastructure.database.entity.UserEntity;
 import com.example.backend.infrastructure.database.entity.WebPushSubscriptionEntity;
+import com.example.backend.infrastructure.database.entity.enums.NotificationSeverity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,7 +17,10 @@ import java.util.Optional;
 public interface WebPushSubscriptionRepository extends JpaRepository<WebPushSubscriptionEntity, String> {
 
     @Query("SELECT w FROM WebPushSubscriptionEntity w WHERE w.userEntity IN :users AND w.isActive = true AND w.severityLevel > :severityLevel")
-    List<WebPushSubscriptionEntity> findAllByUserEntityInAndActiveIsTrue(List<UserEntity> users, int severityLevel);
+    List<WebPushSubscriptionEntity> findAllByUserEntityInAndActiveIsTrue(
+            List<UserEntity> users,
+            NotificationSeverity severityLevel
+    );
 
     @Query("SELECT w FROM WebPushSubscriptionEntity w WHERE w.userEntity.id = ?1")
     List<WebPushSubscriptionEntity> findByUserId(@Param("id") String id);
